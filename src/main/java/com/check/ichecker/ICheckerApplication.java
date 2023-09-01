24fc736b540e980ebd38223695a29a767f55be69
ggyu0629 <ksg1885@naver.com>
2023-09-01 17:17:43 +0900

    project init

---
+++ src/main/java/com/check/ichecker/ICheckerApplication.java
@@ -0,0 +1,13 @@
+package com.check.ichecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/check/ichecker/ICheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.check.ichecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
